ef9482f037c148b2e95eb266aebcc18471792712
busesavci245 <busesavci@hotmail.com>
2018-07-26 19:41:12 +0300

    Add some pages :unicorn:

--- elifinokuzu/dictionary/views.py
+++ elifinokuzu/dictionary/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from dictionary.models import Node
+from django.shortcuts import render, redirect
+from dictionary.models import Node,Edge
+from django.urls import reverse
+from .forms import SubmissionForm
+
 
 def home(request):
     nodes = Node.objects.all()
@@ -22,3 +25,35 @@ def node_detail(request, id):
         'outgoing': outgoing,
         'title': 'Öküzün Elifi: %s' % node.name,
     })
+
+def about(request):
+    return render(request, 'about.html')
+
+def support(request):
+    return render(request, 'support.html')
+
+def submit(request):
+    form = SubmissionForm()
+
+    if request.method == "POST":
+        form=SubmissionForm(request.POST)
+
+        if form.is_valid():
+            source_node = Node.objects.create(
+                name=form.cleaned_data['source_node'],
+                language=form.cleaned_data['source_language'],
+            )
+            target_node = Node.objects.create(
+                name=form.cleaned_data['target_node'],
+                language=form.cleaned_data['target_language'],
+            )
+            edge = Edge.objects.create(
+                source=source_node,
+                destination=target_node,
+                is_directed=False,
+                type_of_edge=form.cleaned_data['type_of_edge'],
+                resource=form.cleaned_data['resource'],
+            )
+            return redirect(reverse("node_detail", args=[source_node.id]))
+
+    return render(request, 'submit.html',{"form":form})
--- elifinokuzu/elifinokuzu/urls.py
+++ elifinokuzu/elifinokuzu/urls.py
@@ -14,11 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from dictionary import views
+from django.urls import path, include
+"""
+üstteki import bunları getirecek
+accounts/login/ [name='login']
+accounts/logout/ [name='logout']
+accounts/password_change/ [name='password_change']
+accounts/password_change/done/ [name='password_change_done']
+accounts/password_reset/ [name='password_reset']
+accounts/password_reset/done/ [name='password_reset_done']
+accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/reset/done/ [name='password_reset_complete']"""
+from dictionary import views as dictionary_views
+from accounts import views as account_views
+
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', dictionary_views.home, name='home'),
+    path('about/', dictionary_views.about, name='about'),
+    path('support/', dictionary_views.support, name='support'),
     path('admin/', admin.site.urls),
-    path('nodes/<int:id>/', views.node_detail, name="node_detail"),
+    path('nodes/<int:id>/', dictionary_views.node_detail, name="node_detail"),
     # node_detail.html dosyasının yolunu verdik
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    # 3. parti paket üstteki gibi dahil ediliyor
+    path('accounts/signup/',account_views.signup, name='signup'),
+    path('accounts/profile/',account_views.dashboard, name='dashboard'),
+    path('submit/',dictionary_views.submit, name='submit'),
+    ]
